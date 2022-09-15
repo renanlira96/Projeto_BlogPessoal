2f351136f50ff2a5f75ed1d81f9e40fda5b7705e
renanlira96 <renanlira2@gmail.com>
2022-09-15 15:38:55 -0300

    Test Unitário JUnit.

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -41,6 +41,22 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 	
+	
+	public Usuario(Long id, @NotNull(message = "O Atributo Nome é Obrigatório!") String nome,
+			@NotNull(message = "O Atributo Usuário é Obrigatório!") @Email(message = "O Atributo Usuário deve ser um email válido!") String usuario,
+			@NotBlank(message = "O Atributo Senha é Obrigatório!") @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres") String senha,
+			@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres") String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+
+	
+
+	public Usuario() { }
+	
 	public Long getId() {
 		return id;
 	}
